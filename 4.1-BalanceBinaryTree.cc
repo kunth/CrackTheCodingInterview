b8ffe717aaa9153ebb035c41e1ad42075c1f0411
axegang <fibonacci@zju.edu.cn>
2014-06-09 14:37:08 +0800

    update Third trial of BalanceBinaryTree

--- 4.1-BalanceBinaryTree.cc
+++ 4.1-BalanceBinaryTree.cc
@@ -10,7 +10,7 @@
 * };
 */
 
-//SECOND TRIAL
+//SECOND TRIAL, 60ms
 class Solution {
 private:
     int getHeight(TreeNode* root)
@@ -26,5 +26,30 @@ public:
         int lh = getHeight(root->left);
         int rh = getHeight(root->right);
         return abs(lh-rh)<=1 && isBalanced(root->left) && isBalanced(root->right);
+    }
+};
+
+//Third Trial, 88ms, unbelievalbe, slower than last one
+class Solution {
+private:
+    int getHeight(TreeNode* root)
+    {
+        if(!root)
+            return 0;
+        int lh = getHeight(root->left);
+        if(lh == -1)
+            return -1;
+        int rh = getHeight(root->right);
+        if(rh == -1)
+            return -1;
+        if(abs(lh-rh)>1)
+            return -1;
+        return 1 + max(lh, rh);
+    }
+public:
+    bool isBalanced(TreeNode *root) {
+        if(getHeight(root)==-1)
+            return false;
+        return true;
     }
 };
